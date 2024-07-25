cc71c05d63eef36282c5f2c6e39cbabcf905a74b
rgblotus <dell.rajeev@outlook.com>
2024-07-25 19:18:58 +0530

    Project Setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # link
+
+pipenv shell
+
+pip install django
+
+django-admin startproject projectname
+django-admin startapp appname
+
---
+++ hcnp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HcnpConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "hcnp"
